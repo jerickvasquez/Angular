61df88d835567e564174a11e6464fd1a329e3be5
jerickvasquez <jorgeerick446@gmail.com>
2022-03-25 14:50:39 -0500

    Utilizando firebase para la automatizacion

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -6,22 +6,32 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
   providedIn: 'root',
 })
 export class InfoPaginaService {
-
-
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
-    console.log('Serivicio de infoPagina Listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp: InfoPagina) => {
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
 
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-          
-    });
+  private cargarEquipo() {
+    //Leer el archivo JSON
+    this.http
+      .get('https://angular-html-6fdb1-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
   }
 }
